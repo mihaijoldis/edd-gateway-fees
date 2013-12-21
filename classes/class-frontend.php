1b9ce319581bc95eaabad0169cfeadb0b1df54fb
Pippin Williamson <pippin@pippinsplugins.com>
2013-12-20 22:10:20 -0600

    Fix issue with flat fee not applying if percentage fee is not enabled

--- classes/class-frontend.php
+++ classes/class-frontend.php
@@ -18,23 +18,36 @@ class EDD_GF_Frontend {
 		global $edd_options;
 		// get total
 		$total = edd_get_cart_total();
+		
 		// which gateway
 		$gateway = edd_get_chosen_gateway();
+		
 		// apply % if appl
-			$percent =  edd_get_option('edd_gf_percent_'.$gateway,'');
-			// sanitize percent
-			$percent = preg_replace('/[^\\d.]+/', '', $percent);
-			$fee = 0;
-			if ( !empty($percent) ){
-				$fee = ($total * (1+($percent/100))) - $total;
-			}
+		$percent =  edd_get_option('edd_gf_percent_'.$gateway,'');
+		
+		// sanitize percent
+		$percent = preg_replace('/[^\\d.]+/', '', $percent);
+		
+		$fee = 0;
+		
+		if ( !empty($percent) ){
+			$percent_fee = ($total * (1+($percent/100))) - $total;
+		}
+		
 		// apply flat if appl
-			$flat = edd_get_option('edd_gf_flat_'.$gateway,'');
-			// sanitize flat
-			$flat = preg_replace('/[^\\d.]+/', '', $flat);
-			if ( !empty($percent) ){
-				$fee = $fee + $flat;
-			}
+		$flat = edd_get_option('edd_gf_flat_'.$gateway,'');
+		
+		// sanitize flat
+		$flat = preg_replace('/[^\\d.]+/', '', $flat);
+		
+		if ( !empty( $percent_fee ) ) {
+			$fee += $percent_fee;
+		}
+
+		if ( ! empty( $flat ) ) {
+			$fee += $flat;
+		}
+		
 		// return total
 		return $fee;
 		
