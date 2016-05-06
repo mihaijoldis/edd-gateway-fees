bf3a90e135f01553d6932a89e479fd46ae4ce5b1
Pippin Williamson <pippin@pippinsplugins.com>
2016-05-06 12:50:19 -0500

    Version bump and changelog for 1.5

--- edd-gateway-fees.php
+++ edd-gateway-fees.php
@@ -6,7 +6,7 @@
  * Author:              Chris Christoff
  * Author URI:          http://www.chriscct7.com
  *
- * Version:             1.4.4
+ * Version:             1.5
  * Requires at least:   3.7
  * Tested up to:        4.5
  *
@@ -66,7 +66,7 @@ class EDD_GF {
 		}
 		// Plugin Version
 		if ( !defined( 'edd_gf_plugin_version' ) ) {
-			define( 'edd_gf_plugin_version', '1.4.4' );
+			define( 'edd_gf_plugin_version', '1.5' );
 		}
 		// Plugin Root File
 		if ( !defined( 'edd_gf_plugin_file' ) ) {
